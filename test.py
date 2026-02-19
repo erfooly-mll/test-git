16b36b1537ac23d26a473af6dadaa8c717a9b2aa
Erfooly <erka29ml@gmil.com>
2026-02-19 08:48:41 -0500

    adding new test after while

--- test.py
+++ test.py
@@ -1,4 +1,2 @@
-print('hi this is  my first modifyn and now we want to do am')
-'add sklearn'
-'add commit to master branch'
-'add commit for test fetch'
+print('hi this is  my first modifyn')
+#hi im back again and i am test the github using my virtual ubunto machine . i connected to my account using ssh key.
